1a3ede14de6ce5a18f10b9cbd2cbe485f63bef33
Skylumi <93416709+SkyLumi@users.noreply.github.com>
2025-04-24 20:09:42 +0700

    tambah route setting

--- api/app.py
+++ api/app.py
@@ -10,6 +10,11 @@ current_moisture = {
     "timestamp": datetime.now().isoformat()
 }
 
+current_setting = {
+    "status_penyiraman_otomatis": "OFF",
+    "batas_kelembaban": 20.0
+}
+
 @app.route('/moisture', methods=['GET'])
 def get_moisture():
     return jsonify(current_moisture)
@@ -23,6 +28,25 @@ def update_moisture():
     current_moisture['moisture'] = data['moisture']
     current_moisture['timestamp'] = datetime.now().isoformat()
     return jsonify({'message': 'Kelembapan berhasil diperbarui', 'data': current_moisture}), 200
+
+@app.route('/setting', methods=['GET'])
+def get_setting():
+    return jsonify(current_setting)
+
+@app.route('/setting', methods=['POST'])
+def update_setting():
+    data = request.get_json()
+    
+    if 'status_penyiraman_otomatis' not in data or 'batas_kelembaban' not in data:
+        return jsonify({'error': 'Payload harus memiliki status_penyiraman_otomatis dan batas_kelembaban'}), 400
+    
+    current_setting['status_penyiraman_otomatis'] = data['status_penyiraman_otomatis']
+    current_setting['batas_kelembaban'] = float(data['batas_kelembaban'])
+
+    return jsonify({
+        'message': 'Pengaturan berhasil diperbarui',
+        'data': current_setting
+    }), 200
 
 if __name__ == '__main__':
     app.run(debug=False)
